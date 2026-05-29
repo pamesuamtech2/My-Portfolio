e52c8208fe8b2ef5e0c029d14cc30cc2fabdf757
Pamela Suarez <paracuentaspomme@gmail.com>
2026-05-29 12:32:44 -0600

    commit darkmode

--- nombres.js
+++ nombres.js
@@ -1,39 +1,24 @@
-NavArea:
-LinkNavA - "aboutme.html" 
-LinkNavB - "projects.html"
-LinkNavC - "skills.html"
-LinkNavD - "contact.html"
+const professionalSkills = [
+    id: SKILL1
+      <img src=>
+    
+    id: SKILL2
+      <img src=>
+    
+    id: SKILL3      
+      <img src=>
+]
 
-MyName: "Pamela Suárez"
-MyRole: "Frontend Developer and Digital Artist"
-
-Picture: "./assets/Sin título 142_202605080716_40540.png"
-LinkProjects1: "projects.html"
-
-Description: "WhoIAm, I am a passionate..."
-LinkProjects2: "projects.html"
-
-MySkills:
+let MySkills:
 SkillHTML: "./assets/free-html-3d-icon-png-download-3640299.webp"
 SkillCSS: "./assets/png-clipart-blue-and-white-logo-web-development-responsive-web-design-cascading-style-sheets-html-css-blue-angle-thumbnail-removebg-preview.png"
 SkillJS: "./assets/javascript-logo-javascript-icon-transparent-free-png.webp"
 
 LinkResume: "resume.pdf"
 
-- AdditionalSkills -
+let AdditionalSkills:
 ImgNASA: "assets/WhatsApp Image 2026-05-09 at 6.11.17 PM.png"
 ImgEnglish:"assets/WhatsApp Image 2026-05-09 at 6.59.06 PM.png" 
 ImgPromptEngineering:"assets/1778376452819.png"
 ImgAdoveIlustrator<"assets/Illustrator-logo.png"
 
-Subtittle: "The works closest to my heart" 
-LinkProjects3: "projects.html"
-LastPicture1: "./assets/WhatsApp Image 2026-05-07 at 3.45.10 PM.jpeg"
-LastPicture2: "./assets/IMG_20230410_094320~2.jpg"
-LastPicture3: "./assets/IMG_20230410_170509~3.jpg"
-
-FormTittle: "Lets have a chat"
-FormName: "Your name"
-FormEmail: "Email address"
-FormText: "Chat here"
-FormButton: "Submit here"
